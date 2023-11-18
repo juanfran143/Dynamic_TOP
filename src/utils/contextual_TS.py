350a0173ece22b1027b65e9b3646ad0067236d77
AntonioRUguina <AntonioRUguina@gmail.com>
2023-11-18 22:29:22 +0100

    imported bayes_logistic, new test

--- src/utils/contextual_TS.py
+++ src/utils/contextual_TS.py
@@ -3,7 +3,8 @@ import numpy as np
 from scipy.optimize import minimize
 import time
 import random
-
+#from bayes_logistic import *
+import matplotlib.pyplot as plt
 
 # This is the blackbox class, bandidts = types
 class ContextualMAB:
@@ -13,8 +14,11 @@ class ContextualMAB:
     def __init__(self):
         # we build two bandits
         self.weights = {}
-        self.weights[0] = [0, -0.8, -0.3, -1.5]
-        self.weights[1] = [0.7, -0.8, -0.3, -1.5]
+        self.weights[0] = [0.7, -0.8, -0.3, -2.5]
+        self.weights[1] = [0, -0.55, -0.2, -2]
+        self.weights[2] = [0.9, 0, -0.15, -0.5]
+        self.weights[3] = [1, 0.4, -0.1, -0.25]
+        self.weights[4] = [1.1, 0.6, -0.05, -0.1]
 
     # method for acting on the bandits
     def draw(self, k, x):
@@ -72,6 +76,9 @@ class OnlineLogisticRegression:
         # initializing weights
         self.w = np.random.normal(self.m, self.alpha * (self.q) ** (-1.0), size=self.n_dim)
 
+    def simulate(self, weather, congestion, battery) -> float:
+        pass
+
     # the loss function
     def loss(self, w, *args):
         X, y = args
@@ -90,10 +97,14 @@ class OnlineLogisticRegression:
 
     # fitting method
     def fit(self, X, y):
-
+        """
+        :param X: nparray (N,3)
+        :param y: np ( N,1)
+        :return: float
+        """
         # step 1, find w
         self.w = minimize(self.loss, self.w, args=(X, y), jac=self.grad, method="L-BFGS-B",
-                          options={'maxiter': 20, 'disp': True}).x
+                          options={'maxiter': 20, 'disp': False}).x
         self.m = self.w
 
         # step 2, update q
@@ -124,6 +135,66 @@ class OnlineLogisticRegression:
 
 
 if __name__ == "__main__":
+    np.random.seed(123)
+    # cmab3 is blackbox
+    cmab3 = ContextualMAB()
+
+    # These are the 3 variables, if the range is positive, BAD
+    X1 = np.random.choice([-1, +1], size=20)
+    X2 = np.random.choice([-1, +1], size=20)
+    X3 = np.random.uniform(-1, 1, 20)
+    X = np_combined_array = np.column_stack((X1, X2, X3))
+    # cmb3.draw is to simulate in blacbox
+    y = np.array([cmab3.draw(1, X[i])[0] for i in range(0, len(X1))])
+
+    # OLR object
+    online_lrn3 = OnlineLogisticRegression(0.5, 1, 3)
+    x_res = X.reshape(-3, 3)
+    # start = time.time()
+    online_lrn3.fit(x_res, y)
+    # end = time.time()
+    # print(end - start)
+
+    # Now test the capacity of prediction with a sample of 10
+
+    X1 = np.random.choice([-1, 1], size=2)
+    X2 = np.random.choice([-1, 1], size=2)
+    X3 = np.random.uniform(-1, 1, 2)
+    X_test = np_combined_array = np.column_stack((X1, X2, X3))
+    x_test = X.reshape(-3, 3)
+    yprueba = np.array([cmab3.prob(1, X_test[i]) for i in range(0, len(X1))])
+
+    print(yprueba)
+
+    print(online_lrn3.predict_proba(X_test, "sample"))
+    X1 = np.random.choice([-1, +1], size=20)
+    X2 = np.random.choice([-1, +1], size=20)
+    X3 = np.random.uniform(-1, 1, 20)
+    X = np_combined_array = np.column_stack((X1, X2, X3))
+    x_res = X.reshape(-3, 3)
+    online_lrn3.fit(x_res, y)
+
+
+
+
+
+    """"
+    np.random.seed(12345)
+    X1 = np.random.choice([0, 1], size=100)
+    X2 = np.random.choice([0, 1], size=100)
+    X3 = np.random.uniform(0, 1, 100)
+    X = np.column_stack((X1, X2, X3))
+    yprueba = np.array([cmab3.prob(1, X[i]) for i in range(0, len(X1))])
+
+    b = b_log.predict_proba(X)
+    #print(yprueba,b)
+    mse= 0
+    for i in range(0,len(b)):
+        mse = (b[i][0] - yprueba[i][0])**2
+    mse = mse/len(b)
+
+    print(mse)
+
 
     # instance of our class
     np.random.seed(123)
@@ -131,12 +202,12 @@ if __name__ == "__main__":
     cmab3 = ContextualMAB()
 
     #These are the 3 variables, if the range is positive, BAD
-    X1 = np.random.choice([-1, +1], size=10000)
-    X2 = np.random.choice([-1, +1], size=10000)
-    X3 = np.random.uniform(-1, 1, 10000)
+    X1 = np.random.choice([-1, +1], size=5000)
+    X2 = np.random.choice([-1, +1], size=5000)
+    X3 = np.random.uniform(-1, 1, 5000)
     X = np_combined_array = np.column_stack((X1, X2, X3))
     # cmb3.draw is to simulate in blacbox
-    y = np.array([cmab3.draw(0, X[i])[0] for i in range(0, len(X1))])
+    y = np.array([cmab3.draw(1, X[i])[0] for i in range(0, len(X1))])
 
 
     # OLR object
@@ -153,6 +224,51 @@ if __name__ == "__main__":
     X2 = np.random.choice([-1, 1], size=10)
     X3 = np.random.uniform(-1, 1, 10)
     X = np_combined_array = np.column_stack((X1, X2, X3))
-    yprueba = np.array([cmab3.prob(0, X[i]) for i in range(0, len(X1))])
+    yprueba = np.array([cmab3.prob(1, X[i]) for i in range(0, len(X1))])
     print(yprueba)
     print(online_lrn3.predict_proba(X,"sample"))
+
+
+
+
+ cmab3 = ContextualMAB()
+    X1 = np.random.choice([0, 1], size=10)
+    X2 = np.random.choice([0, 1], size=10)
+    X3 = np.random.uniform(0, 1, 10)
+    X = np.column_stack((X1, X2, X3))
+    y = np.array([cmab3.draw(0, X[i])[0] for i in range(0, len(X1))])
+    results = []
+
+    X1_r = np.random.choice([0, 1], size=1000)
+    X2_r = np.random.choice([0, 1], size=1000)
+    X3_r = np.random.uniform(0, 1, 1000)
+    X_r = np.column_stack((X1_r, X2_r, X3_r))
+    yprueba = np.array([cmab3.prob(0, X_r[n]) for n in range(0, len(X1_r))])
+
+
+    for i in range(0, 20):
+        b_log = EBLogisticRegression()
+        b_log.fit(X, y)
+        print((i))
+
+        for _ in range(0, i):
+            X1_new = np.random.choice([0, 1], size=10)
+            X2_new = np.random.choice([0, 1], size=10)
+            X3_new = np.random.uniform(0, 1, 10)
+            X_new = np.column_stack((X1_new, X2_new, X3_new))
+            y_new = np.array([cmab3.draw(0, X_new[k])[0] for k in range(0, len(X1_new))])
+            b_log = b_log.fit(X_new, y_new)
+
+        b = b_log.predict_proba(X_r)
+        mse = 0
+        for j in range(0, len(yprueba)):
+            mse += abs(b[j][0] - yprueba[j][0])
+        mse = mse / len(yprueba)
+        results.append(mse)
+
+    print((results))
+    plt.plot(results)
+    plt.show()
+
+"""""
+
