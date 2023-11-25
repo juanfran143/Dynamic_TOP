288f3af0524aed609f4791d880ed757ab5110565
AntonioRUguina <AntonioRUguina@gmail.com>
2023-11-25 11:21:28 +0100

    More Test added

--- src/utils/contextual_test1dim.py
+++ src/utils/contextual_test1dim.py
@@ -1,25 +1,24 @@
-
 import numpy as np
 from scipy.optimize import minimize
 import time
 import random
 from bayes_logistic import *
-
+from scipy.stats import norm as norm_dist
 import matplotlib.pyplot as plt
-#from statsmodels.tools.tools import add_constant
+
+
+# from statsmodels.tools.tools import add_constant
 # This is the blackbox class, bandidts = types
 class ContextualMAB:
 
-#!!!! INTERCEPT ==0 !!!!
+    # !!!! INTERCEPT ==0 !!!!
     # initialization
     def __init__(self):
         # we build two bandits
         self.weights = {}
-        self.weights[0] = [0.7, -0.8, -0.3, -2.5]
-        self.weights[1] = [0, -0.55, -0.2, -2]
-        self.weights[2] = [0.9, 0, -0.15, -0.5]
-        self.weights[3] = [1, 0.4, -0.1, -0.25]
-        self.weights[4] = [1.1, 0.6, -0.05, -0.1]
+        self.weights[0] = [0.7, -3]
+        self.weights[1] = [0, -4]
+
 
     # method for acting on the bandits
     def draw(self, k, x):
@@ -29,7 +28,7 @@ class ContextualMAB:
         # loop for each bandit
         for bandit in self.weights.keys():
             # linear function of external variable
-            f_x = self.weights[bandit][0] + self.weights[bandit][1] * x[0] + self.weights[bandit][1] * x[1] + self.weights[bandit][2] * x[2]
+            f_x = self.weights[bandit][0] + self.weights[bandit][1] * x
 
             # generate reward with probability given by the logistic
             probability = 1 / (1 + np.exp(-f_x))
@@ -39,7 +38,8 @@ class ContextualMAB:
 
         # give reward according to probability
         return np.random.choice([0, 1], p=[1 - prob_dict[k], prob_dict[k]]), max(prob_dict.values()) - prob_dict[k], \
-        prob_dict[k]
+            prob_dict[k]
+
     def prob(self, k, x):
         # probability dict
         prob_dict = {}
@@ -47,8 +47,7 @@ class ContextualMAB:
         # loop for each bandit
         for bandit in self.weights.keys():
             # linear function of external variable
-            f_x = self.weights[bandit][0] + self.weights[bandit][1] * x[0] + self.weights[bandit][1] * x[1] + \
-                  self.weights[bandit][2] * x[2]
+            f_x = self.weights[bandit][0] + self.weights[bandit][1] * x
 
             # generate reward with probability given by the logistic
             probability = 1 / (1 + np.exp(-f_x))
@@ -135,43 +134,65 @@ if __name__ == "__main__":
     # cmab3 is blackbox
     cmab3 = ContextualMAB()
 
-    # These are the 3 variables, if the range is positive, BAD
-    X1 = np.random.choice([-1, +1], size=1000)
-    X2 = np.random.choice([-1, +1], size=1000)
-    X3 = np.random.uniform(-1, 1, 1000)
-    X = np_combined_array = np.column_stack((X1, X2, X3))
-    x_res = X.reshape(-3, 3)
-    # cmb3.draw is to simulate in blacbox
-    y = np.array([cmab3.draw(0, X[i])[0] for i in range(0, len(X1))])
+    # let us fit this model to some data #
+
+    # ground truth
+    X = np.random.uniform(-6, 6, 500)
+    y = np.array([cmab3.draw(1, x)[0] for x in X])
 
     # OLR object
-    online_lrn3 = OnlineLogisticRegression(0.5, 1, 3)
-
-    # start = time.time()
-    online_lrn3.fit(x_res, y)
-    # end = time.time()
-    # print(end - start)
-
+    online_lr = OnlineLogisticRegression(0.5, 1, 1)
+    online_lr.fit(X.reshape(-1, 1), y)
     # Now test the capacity of prediction with a sample of 10
 
-    X1_new = np.random.choice([-1, 1], size=5)
-    X2_new = np.random.choice([-1, 1], size=5)
-    X3_new = np.random.uniform(-1, 1, 5)
-    X_test = np_combined_array = np.column_stack((X1_new, X2_new, X3_new))
-    x_test = X_test.reshape(-3, 3)
-    yprueba = np.array([cmab3.prob(0, X_test[i]) for i in range(0, len(X1_new))])
-    yout = np.array([cmab3.draw(0, X_test[i]) for i in range(0, len(X1_new))])
+    Xprueba = np.random.uniform(-6, 6, 5)
+    yprueba = np.array([cmab3.prob(1, x)[0] for x in Xprueba])
     print(yprueba)
     print("")
 
-    print(online_lrn3.predict_proba(X_test, "sample"))
+    print(online_lr.predict_proba(Xprueba.reshape(-1, 1), "sample"))
 
+    # closing other figures
+    plt.close('all')
 
+    # let us check the distribution of weights and uncertainty bounds
+    plt.figure(figsize=[9, 3.5], dpi=150)
 
+    # plotting the pdf of the weight distribution
+    X_pdf = np.linspace(-4, 4, 1000)
+    pdf = norm_dist(loc=online_lr.m, scale=online_lr.q ** (-1.0)).pdf(X_pdf)
 
+    # range and resolution of probability plot
+    X_prob = np.linspace(-6, 6, 1000)
+    p_dist = 1 / (1 + np.exp(-X_prob * online_lr.m))
+    p_dist_plus = 1 / (1 + np.exp(-X_prob * (online_lr.m + 2 * online_lr.q ** (-1.0))))
+    p_dist_minus = 1 / (1 + np.exp(-X_prob * (online_lr.m - 2 * online_lr.q ** (-1.0))))
 
+    # opening subplots
+    ax1 = plt.subplot2grid((1, 5), (0, 0), colspan=2, rowspan=1)
+    ax2 = plt.subplot2grid((1, 5), (0, 2), colspan=3, rowspan=1)
 
+    # plotting distriution of weights
+    ax1.plot(X_pdf, pdf, color='b', linewidth=2, alpha=0.5)
+    ax1.plot([cmab3.weights[1][1], cmab3.weights[1][1]], [0, max(pdf)], 'k--', label='True $\\beta$', linewidth=1)
+    ax1.fill_between(X_pdf, pdf, 0, color='b', alpha=0.2)
 
+    # plotting probabilities
+    ax2.plot(X_prob, p_dist, color='b', linewidth=2, alpha=0.5)
+    ax2.fill_between(X_prob, p_dist_plus, p_dist_minus, color='b', alpha=0.2)
+    ax2.scatter(X, y, c='k')
+
+    # title and comments
+    ax1.set_title('OLR estimate for $\\beta$', fontsize=10)
+    ax1.set_xlabel('$\\beta$', fontsize=10);
+    ax1.set_ylabel('$density$', fontsize=10)
+    ax2.set_title('OLR estimate for $\\theta(x)$', fontsize=10)
+    ax2.set_xlabel('$x$', fontsize=10);
+    ax2.set_ylabel('$\\theta(x)$', fontsize=10)
+
+    ax1.legend(fontsize=10)
+    plt.tight_layout()
+    plt.show()
 
 
     """"
@@ -180,7 +201,7 @@ if __name__ == "__main__":
     print(yprueba)
     print("")
     print(online_lrn3.predict_proba(X_test, "sample"))
-    
+
         def loss(self, w, *args):
         X, y = args
         #print(w[-1],w[:-1])
@@ -198,14 +219,14 @@ if __name__ == "__main__":
         print(a2)
         return np.concatenate([a1, np.array([a2])])
 
-    
+
      X1 = np.random.choice([-1, +1], size=20)
     X2 = np.random.choice([-1, +1], size=20)
     X3 = np.random.uniform(-1, 1, 20)
     X = np_combined_array = np.column_stack((X1, X2, X3))
     x_res = X.reshape(-3, 3)
     online_lrn3.fit(x_res, y)
-    
+
     np.random.seed(123)
     # cmab3 is blackbox
     cmab3 = ContextualMAB()
@@ -239,15 +260,15 @@ if __name__ == "__main__":
     print(yprueba)
 
     print(online_lrn3.predict_proba(X_test, "sample"))
-    
-    
+
+
     X1 = np.random.choice([-1, +1], size=20)
     X2 = np.random.choice([-1, +1], size=20)
     X3 = np.random.uniform(-1, 1, 20)
     X = np_combined_array = np.column_stack((X1, X2, X3))
     x_res = X.reshape(-3, 3)
     online_lrn3.fit(x_res, y)
-    
+
     np.random.seed(12345)
     X1 = np.random.choice([0, 1], size=100)
     X2 = np.random.choice([0, 1], size=100)
@@ -338,8 +359,8 @@ if __name__ == "__main__":
     print((results))
     plt.plot(results)
     plt.show()
-    
-    
+
+
         def loss(self, w, *args):
         X, y = args
         print(w[-1],w[:-1])
@@ -347,8 +368,8 @@ if __name__ == "__main__":
             [np.log(1 + np.exp(-y[j]*w[-1] - y[j] * w[:-1].dot(X[j]))) for j in range(y.shape[0])]))
         return 0.5 * (self.q * (w[:-1] - self.m_int[:-1])).dot(w[:-1] - self.m_int[:-1]) - np.sum(
             [np.log(1 + np.exp(-y[j]*w[-1] - y[j] * w[:-1].dot(X[j]))) for j in range(y.shape[0])])
-            
-            
+
+
                 def grad(self, w, *args):
         X, y = args
         a1 = self.q * (w[:-1] - self.m) + (-1) * np.array(
