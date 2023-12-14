6fac8ace2c7699dafcf80bae45973445cde28a18
juanfran <juanfrancisco.gomez@servexternos.repsol.com>
2023-12-14 10:51:28 +0100

    automatic bias and delta

--- src/main.py
+++ src/main.py
@@ -37,7 +37,9 @@ def create_instance(instance_dict):
     return Solution(**filtered_args), instance_dict
 
 
-def save_reduced_information_to_txt(info, mean_of, mean_dynamic_of, std_dynamic_of, max_dynamic_of, min_dynamic_of, time, filename="..\\output\\output.txt"):
+def save_reduced_information_to_txt(info, mean_of, mean_dynamic_of, std_dynamic_of, max_dynamic_of,
+                                    min_dynamic_of, time, min_dynamic_of_100, min_dynamic_of_10,
+                                    filename="..\\output\\output.txt"):
     """
     Saves reduced information along with additional mean values to a text file, excluding certain keys.
 
@@ -59,6 +61,8 @@ def save_reduced_information_to_txt(info, mean_of, mean_dynamic_of, std_dynamic_
     reduced_info['std_dynamic_of'] = std_dynamic_of
     reduced_info['max_dynamic_of'] = max_dynamic_of
     reduced_info['min_dynamic_of'] = min_dynamic_of
+    reduced_info['min_dynamic_of_100'] = min_dynamic_of_100
+    reduced_info['min_dynamic_of_10'] = min_dynamic_of_10
 
 
     values_to_save = [v for k, v in reduced_info.items()]
@@ -68,19 +72,61 @@ def save_reduced_information_to_txt(info, mean_of, mean_dynamic_of, std_dynamic_
         file.write(";".join(map(str, values_to_save)) + "\n")
 
 
+def calculate_delta(instance_dict):
+    best_delta = 0
+    best_beta = 0
+    best_value = 0
+    max_iteration_random = instance_dict["max_iter_random"]
+    instance_dict["max_iter_random"] = 50
+    max_iter_dynamic = instance_dict["max_iter_dynamic"]
+    instance_dict["max_iter_dynamic"] = 1
+
+    for delta in range(6, 10):
+        for beta in range(6, 10):
+            instance_dict["delta"] = delta/10
+            instance_dict["beta_bias"] = beta/10
+
+            list_arguments = [Key.NODES, Key.MAX_DIST, Key.SEED, Key.MAX_VEHICLES, Key.ALPHA, Key.NEIGHBOUR_LIMIT,
+                              Key.BLACKBOX,
+                              Key.DICT_OF_TYPE, Key.MAX_ITER_DYNAMIC, Key.MAX_ITER_RANDOM, Key.BETA_BIAS]
+            filtered_args = {key: instance_dict[key] for key in instance_dict if key in list_arguments}
+            solution = Solution(**filtered_args)
+            result = solution.run(Algorithm.CONSTRUCTIVE_STATIC, Algorithm.SELECT_SAVING_GRASP, instance_dict)
+            if best_value < np.mean(result[1]):
+                best_value = np.mean(result[1])
+                best_delta = delta/10
+                best_beta = beta/10
+
+    instance_dict["max_iter_dynamic"] = max_iter_dynamic
+    instance_dict["max_iter_random"] = max_iteration_random
+    instance_dict["delta"] = best_delta
+    instance_dict["beta_bias"] = best_beta
+
+    return instance_dict
+
+
 if __name__ == '__main__':
     instance = read_run()
     for instance_dict in instance:
         solution, instance_dict = create_instance(instance_dict)
         algo = instance_dict[Key.ALGORITHM]
         selected_procedure = instance_dict[Key.SELECTED_NODE_FUNCTION]
+        instance_dict = calculate_delta(instance_dict)
         start = time.time()
+
+        list_arguments = [Key.NODES, Key.MAX_DIST, Key.SEED, Key.MAX_VEHICLES, Key.ALPHA, Key.NEIGHBOUR_LIMIT,
+                          Key.BLACKBOX,
+                          Key.DICT_OF_TYPE, Key.MAX_ITER_DYNAMIC, Key.MAX_ITER_RANDOM, Key.BETA_BIAS]
+        filtered_args = {key: instance_dict[key] for key in instance_dict if key in list_arguments}
+        solution = Solution(**filtered_args)
+
         results = solution.run(algo, selected_procedure, instance_dict)
         save_reduced_information_to_txt(instance_dict, np.mean(results[1]), np.mean(results[2]), np.std(results[2]),
-                                        np.min(results[2]), np.max(results[2]), time.time()-start)
+                                        np.min(results[2]), np.max(results[2]), time.time()-start,
+                                        np.min(results[2][:100]), np.min(results[2][:10]))
 
-        # maps = Map(instance_dict["nodes"])
-        # maps.print_route(results[0][-1])
+        maps = Map(instance_dict["nodes"])
+        maps.print_route(results[0][-1])
 
         print(instance_dict[Key.ALGORITHM])
         print("Mean OF: " + str(np.mean(results[1])))
